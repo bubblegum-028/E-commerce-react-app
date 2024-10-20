41839390dfbf8d9e0f3ae04555270771785e2737
jairehnarambulo <arambulojairehn80@gmail.com>
2024-10-21 01:08:50 +0800

    Created Dashboard and Added Add Product Button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Import Navigate for redirection
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
@@ -28,30 +27,5 @@ const App = () => {
         </Router>
     );
 };
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
->>>>>>> 2a7fa3f58500383cec9ba8461ddbf402bed52622
 
 export default App;
